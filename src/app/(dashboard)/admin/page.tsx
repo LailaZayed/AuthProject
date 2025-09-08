92ab739c7eaeafde229ff66ff9d96d0d1a38a5a7
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2025-09-08 10:15:20 +0200

    styling space page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
-const page = async() => {
+const page = async () => {
   const session = await getServerSession(authOptions);
-  if (session?.user){
-    return <h2 className = "text-2xl"> Welcome back to your Space {session?.user.username}</h2>
+
+  if (session?.user) {
+    return (
+      <h2 className="text-4xl font-bold text-pink-600 rounded-lg hover:text-pink-700 transition-colors">
+        Welcome back to your Space, {session?.user.username}
+      </h2>
+    );
   }
 
-  return <h2 className = "text-2xl">Please Login to Load this page</h2>;
-  
+  return (
+    <h2 className="text-4xl font-bold text-pink-600 rounded-lg hover:text-pink-700 transition-colors">
+      Please Login to Load this page
+    </h2>
+  );
 };
 
 export default page;
+
