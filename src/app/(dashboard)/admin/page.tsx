3b5c50f4bce395b5c23f160a2b94ac77770ec42d
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2025-09-08 07:55:36 +0200

    (Admin Page) Welcoming and styling after login

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,9 +3,12 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 const page = async() => {
-  const session = await getServerSession(authOptions)
-  
-  return <div>Welcome to Admin Page</div>
+  const session = await getServerSession(authOptions);
+  if (session?.user){
+    return <h2 className = "text-2xl"> Welcome back to your Space {session?.user.username}</h2>
+  }
+
+  return <h2 className = "text-2xl">Please Login to Load this page</h2>;
   
 };
 
