908908701e5573068a4f12779478d9d6d9132c88
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2025-09-08 10:15:36 +0200

    styling main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
-import {buttonVariants} from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-4xl'>Home</h1>
-      <Link className = {buttonVariants()} href='/admin' > Go to My Space </Link>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full bg-pink-50 space-y-6">
+      <h1 className='text-6xl font-bold text-pink-600 rounded-lg hover:text-pink-700 transition-colors'>
+        Fairy World
+      </h1>
+      <Link 
+        className={`${buttonVariants()} bg-pink-600 text-white hover:bg-pink-700`}
+        href='/admin'
+      >
+        Open my Space
+      </Link>
     </div>
-
   );
-  
 }
