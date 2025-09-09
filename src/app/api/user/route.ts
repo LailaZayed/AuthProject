724387309212f0a792dd15b1febc6c1874db5605
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2025-09-09 19:54:03 +0200

    editing back end validation

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,19 +4,11 @@ import { NextResponse } from "next/server";
 import { hash } from 'bcrypt';
 import * as z from 'zod';
 
-// Define schema for input validation
-
-const userSchema = z
-  .object({
-    username: z.string().min(1, 'Username is required').max(100),
-    email: z.string().min(1, 'Email is required').email('Invalid email'),
-    password: z
-      .string()
-      .min(1, 'Password is required, min length 8 characters')
-      .min(8, 'Password must have than 8 characters'),
-  })
-
-
+const userSchema = z.object({
+  username: z.string().min(1, 'Username is required').max(100),
+  email: z.string().min(1, 'Email is required').email('Invalid email'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+})
 
 
 export async function POST(req: Request){
