dbbf20ee2a543e1066fe7e98246b1e587f29bb38
LailaZayed <s-layla.zayed@zewailcity.edu.eg>
2025-09-09 19:07:35 +0200

    FrontEdit: showing pass required length to user

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -12,7 +12,7 @@ const userSchema = z
     email: z.string().min(1, 'Email is required').email('Invalid email'),
     password: z
       .string()
-      .min(1, 'Password is required')
+      .min(1, 'Password is required, min length 8 characters')
       .min(8, 'Password must have than 8 characters'),
   })
 
